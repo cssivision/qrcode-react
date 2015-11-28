9d31d9767f4d28fb18b22323f053fb8968bf526a
cssivision <cssivision@gmail.com>
2015-11-28 19:06:21 +0800

    example support chinese

--- example/bundle.js
+++ example/bundle.js
@@ -185,8 +185,29 @@ var QRCode = React.createClass({
     this.update();
   },
 
+  utf16to8: function (str) {
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for (i = 0; i < len; i++) {
+      c = str.charCodeAt(i);
+      if (c >= 0x0001 && c <= 0x007F) {
+        out += str.charAt(i);
+      } else if (c > 0x07FF) {
+        out += String.fromCharCode(0xE0 | c >> 12 & 0x0F);
+        out += String.fromCharCode(0x80 | c >> 6 & 0x3F);
+        out += String.fromCharCode(0x80 | c >> 0 & 0x3F);
+      } else {
+        out += String.fromCharCode(0xC0 | c >> 6 & 0x1F);
+        out += String.fromCharCode(0x80 | c >> 0 & 0x3F);
+      }
+    }
+    return out;
+  },
+
   update: function () {
-    var qrcode = qr(this.props.value);
+    var value = this.utf16to8(this.props.value);
+    var qrcode = qr(value);
     var canvas = getDOMNode(this.refs.canvas);
 
     var ctx = canvas.getContext('2d');
